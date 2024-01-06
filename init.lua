bdfc126f5b1f676d3697ca54ccb85b101efe514a
matveybobrov <matveybobrov01@gmail.com>
2024-01-06 20:03:59 +0300

    feat: add a lazy.nvim plugin manager

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Installs a plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
